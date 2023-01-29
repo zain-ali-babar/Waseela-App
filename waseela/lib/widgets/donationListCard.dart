0c7600772b08dac3858409072576ec1012f5fcb4
Ali Mansoor Alvi <56601116+alimansoor767@users.noreply.github.com>
2023-01-30 00:26:18 +0500

    case description edit

--- waseela/lib/widgets/donationListCard.dart
+++ waseela/lib/widgets/donationListCard.dart
@@ -12,12 +12,31 @@ class DonationListCard extends StatefulWidget {
 }
 
 class _DonationListCardState extends State<DonationListCard> {
+  late String firstHalf;
+  late String secondHalf;
+
+  bool flag = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.donationCase.description.length > 100) {
+      firstHalf = widget.donationCase.description.substring(0, 100);
+      secondHalf = widget.donationCase.description
+          .substring(50, widget.donationCase.description.length);
+    } else {
+      firstHalf = widget.donationCase.description;
+      secondHalf = "";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData mediaQuery = MediaQuery.of(context);
     String category = "";
     if (widget.donationCase.category == "Monthly") {
-      category = " /month";
+      category = "/month";
     } else if (widget.donationCase.category == "Target") {
       category = " required";
     }
@@ -43,12 +62,43 @@ class _DonationListCardState extends State<DonationListCard> {
             Padding(
               padding:
                   EdgeInsets.symmetric(vertical: mediaQuery.size.height * 0.01),
-              child: Text(
-                widget.donationCase.description,
-                style: TextStyle(
-                    color: Colors.indigo[900],
-                    fontSize: mediaQuery.size.height * 0.015),
-              ),
+              child: secondHalf.isEmpty
+                  ? Text(
+                      firstHalf,
+                      style: TextStyle(
+                          color: Colors.indigo[900],
+                          fontSize: mediaQuery.size.height * 0.015),
+                    )
+                  : Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "$firstHalf...",
+                          style: TextStyle(
+                              color: Colors.indigo[900],
+                              fontSize: mediaQuery.size.height * 0.015),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical:mediaQuery.size.height*0.002),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              "Read more",
+                              style: TextStyle(
+                                  color: Colors.grey[600],
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: mediaQuery.size.height * 0.015),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+              // child: Text(
+              //   widget.donationCase.description,
+              //   style: TextStyle(
+              //       color: Colors.indigo[900],
+              //       fontSize: mediaQuery.size.height * 0.015),
+              // ),
             ),
             Row(
               children: [
@@ -74,9 +124,9 @@ class _DonationListCardState extends State<DonationListCard> {
                         onPressed: () {},
                         elevation: 2,
                         shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                        color:Colors.white,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        color: Colors.white,
                         child: Text(
                           "Donate",
                           style: TextStyle(
